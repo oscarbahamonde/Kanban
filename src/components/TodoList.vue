666a15550ae527e062f63c5c8a8931d964569362
oscarbahamonde <dev@oscarbahamonde.cloud>
2021-12-30 16:04:52 -0500

    2

---
+++ src/components/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <di>
+    <h1>Todos</h1>
+    <ul>
+      <li v-for="todo of todos" :key="todo.id">
+        {{ todo.name }}
+      </li>
+    </ul>
+  </di>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      todos: []
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get(`http://localhost:3000/todos`);
+
+      this.todos = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: aqua;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  color: #f0f;
+}
+</style>
